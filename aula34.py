159edcf07dddcdfb2ee521ce1111f720f1521cbb
Raphael Santana <raphaelmontanha57@gmail.com>
2026-03-06 17:13:23 -0400

    adiciona aula 34 - estrutura de repetição while

---
+++ aula34.py
@@ -0,0 +1,18 @@
+#numero = int(input("Digite um número: "))
+
+#while numero < 10:
+#    print("O número é menor que 10.")
+#    numero = int(input("Digite um número: "))
+    
+#    if numero == 00:
+#        print("Número 00 digitado. Encerrando o programa.")
+#        break
+
+contador = input("Digite um número para iniciar a contagem: ")
+contador = int(contador)
+
+while contador <= 50:
+    print(contador)
+    contador = contador + 1
+    #contador += 1
+print("Contagem finalizada.")

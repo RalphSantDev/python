a8901bae0f33fad9e4d774edea0aaa798aa54186
Raphael Santana <raphaelmontanha57@gmail.com>
2026-03-10 20:47:35 -0400

    adiciona exemplo de iteração sobre strings e números no arquivo aula43.py

---
+++ aula43.py
@@ -0,0 +1,15 @@
+''' 
+#iterando sobre uma string com for 
+
+string = 'python'
+
+for letra in string:
+    print(letra)
+
+'''
+# percorrendo numeros com for e range
+numeros = range(10)
+
+for numero in numeros:
+    print(numero)
+
